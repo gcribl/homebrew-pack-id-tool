acf884b03dbf0e425e4ebad6d32c8bbc672947d4
Giovanni Mola <gmola@cribl.io>
2025-12-17 15:22:22 -0800

    Update pack-cli to version 1.0.1
    adding shell completion

--- pack-id-tool.rb
+++ pack-id-tool.rb
@@ -3,14 +3,18 @@ class PackIdTool < Formula
 
   desc "CLI tool to modify Cribl pack IDs in .crbl files"
   homepage "https://github.com/gcribl/pack-cli"
-  url "https://github.com/gcribl/pack-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "992078c38950f764e76474a92a90eb2aca19981ada1a135b93bba10ed1d04342"
+  url "https://github.com/gcribl/pack-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
+    # Install shell completions
+    bash_completion.install "completions/pack-id.bash" => "pack-id"
+    zsh_completion.install "completions/_pack-id"
+    fish_completion.install "completions/pack-id.fish"
   end
 
   test do
